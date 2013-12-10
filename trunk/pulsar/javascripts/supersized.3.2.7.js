290262d0660c58b40c9621cdde4ecef1c151619a
nelsonwong@gmail.com <nelsonwong@gmail.com@b5e4cbef-8e27-a3f2-b8b8-98933114dbe7>
2013-12-10 22:23:18 +0000

    removed loader icon

--- trunk/pulsar/javascripts/supersized.3.2.7.js
+++ trunk/pulsar/javascripts/supersized.3.2.7.js
@@ -15,7 +15,9 @@
 	/* Place Supersized Elements
 	----------------------------*/
 	$(document).ready(function() {
-		$('body').append('<div id="supersized-loader"></div><ul id="supersized"></ul>');
+		//$('body').append('<div id="supersized-loader"></div><ul id="supersized"></ul>');
+		$('body').append('<ul id="supersized"></ul>');
+
 	});
     
     
